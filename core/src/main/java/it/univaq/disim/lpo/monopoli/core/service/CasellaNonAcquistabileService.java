2b16f2bf05676ef4b45da81f8ce3832c665518ab
ScappaMarco <mscappa0@gmail.com>
2025-03-29 17:18:34 +0100

    test2
    Signed-off-by: ScappaMarco <mscappa0@gmail.com>

--- core/src/main/java/it/univaq/disim/lpo/monopoli/core/service/CasellaNonAcquistabileService.java
+++ core/src/main/java/it/univaq/disim/lpo/monopoli/core/service/CasellaNonAcquistabileService.java
@@ -3,5 +3,5 @@ package it.univaq.disim.lpo.monopoli.core.service;
 import it.univaq.disim.lpo.monopoli.core.datamodel.CasellaNonAcquistabile;
 
 public interface CasellaNonAcquistabileService<T extends CasellaNonAcquistabile> extends CasellaService<T> {
-    //....
+    //...
 }
